58447a6dd8367a1acfb9933f623d7bcc91fc4f01
ErandyR <rerandy15@gmail.com>
2025-09-11 18:08:38 -0600

    W02 Learning Activity: The Fetch API

---
+++ scripts/prophets.js
@@ -0,0 +1,40 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let dob = document.createElement('p');
+        let placeOfBirth = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('class', 'prophet-img');
+        card.setAttribute('class', 'card')
+
+
+        card.appendChild(fullName);
+        card.appendChild(dob);
+        card.appendChild(placeOfBirth);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
