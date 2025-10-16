b7affd17ec1f42bb70716bc4c99d34ce0201ac89
ErandyR <rerandy15@gmail.com>
2025-10-16 00:28:44 -0600

    api changes

--- final/scripts/quotes.js
+++ final/scripts/quotes.js
@@ -1,7 +1,7 @@
 const quoteTxt = document.getElementById("quote-text");
 const quoteContent = document.getElementById("quote-author");
 
-const url_api = "https://zenquotes.io/api/random";
+const url_api = "https://api.allorigins.win/get?url=https://zenquotes.io/api/random";
 
 const container = document.querySelector(".quote-container");
 
